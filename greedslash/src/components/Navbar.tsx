f5907f53de22532de77358247adac23288cf6bd4
rendersomejuice <rendersomejuice@gmail.com>
2026-09-02 15:45:36 +0200

    added react haslink

--- greedslash/src/components/Navbar.tsx
+++ greedslash/src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import style from '../style.module.css'
 import {Link} from 'react-router-dom'
 
+import { HashLink } from 'react-router-hash-link';
+
 function Navbar () {
 
     return(
         <div className={style.NavbarContainer}>
             <div><Link to="/">HOME</Link></div>
-            <div><a href="#mechanics">MECHANICS</a></div>
-            <div><a href="#characters">CHARACTERS</a></div>
+            <div><HashLink to="/#mechanics">MECHANICS</HashLink></div>
+            <div><HashLink to="/#characters">CHARACTERS</HashLink></div>
             <div><Link to="Devlog">DEVLOG</Link></div>
         </div>
     );
