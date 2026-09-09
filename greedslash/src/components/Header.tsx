6709a59c16d38dbd8ba20c02e3b3c3594508b21f
rendersomejuice <rendersomejuice@gmail.com>
2026-09-09 19:11:27 +0200

    .

--- greedslash/src/components/Header.tsx
+++ greedslash/src/components/Header.tsx
@@ -27,8 +27,8 @@ function Header(){
             <div className={style.steamContainer}>
                 <div className={style.horizontalLayout}>
                     <FaSteam className={style.steamLogo}> size={200}</FaSteam>
-                    <a className={style.wishListButton} target="_blank" href={isMobileDevice?urlMobile:urlPC + "4521520"}>WISHLIST</a>
-                    <a className={style.wishListButton} target="_blank" href={isMobileDevice?urlMobile:urlPC + "4839830"}>PLAY DEMO</a>
+                    <a className={style.wishListButton} target="_blank" href={(isMobileDevice?urlMobile : urlPC) + "4521520"}>WISHLIST</a>
+                    <a className={style.wishListButton} target="_blank" href={(isMobileDevice?urlMobile : urlPC) + "4839830"}>PLAY DEMO</a>
                 </div>
             </div>
         </div>
